86e550f7291ff7a19fe46a31214d8b71f16edb3f
Lea <leandrotosunian@hotmail.com>
2024-04-05 13:51:41 -0300

    logout route test

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -1,7 +1,7 @@
 import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(res: NextResponse) {
+export async function POST(req: NextRequest,res: NextResponse) {
     cookies().delete('sacaturno_token')
     cookies().delete('sacaturno_userID')
     return NextResponse.json({msg: 'LOGGED_OUT'})
