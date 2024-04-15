c836a230cf2bb083b535728b9705617f25fe04f2
Lea <leandrotosunian@hotmail.com>
2024-04-15 18:10:56 -0300

    image on search results fixed

--- app/business/search/page.tsx
+++ app/business/search/page.tsx
@@ -17,7 +17,7 @@ const SearchBusiness: React.FC = () => {
   const router = useRouter()
 
   const myLoader = ({ src }: { src: string }) => {
-    return `${process.env.BACKEND_URL}/api${src}`;
+    return `https://sacaturno-server-production.up.railway.app/api${src}`;
   };
 
   const hideAlert = () => {
