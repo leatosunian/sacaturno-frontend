bb3b06c8745f72154517ea1d26e5dce7afe6aae4
Lea <leandrotosunian@hotmail.com>
2024-04-23 11:02:50 -0300

    spinner test

--- components/BookAppointmentModal.tsx
+++ components/BookAppointmentModal.tsx
@@ -7,6 +7,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { FieldValues, useForm } from "react-hook-form";
 import { bookAppointmentSchema } from "@/app/schemas/bookAppointmentSchema";
 import { IoMdClose } from "react-icons/io";
+import Spinner from "../../chat-typescript/frontend/src/components/Spinner";
 
 interface eventType2 {
   start: string;
@@ -32,9 +33,11 @@ interface formInputs {
 const BookAppointmentModal: React.FC<props> = ({
   appointmentData,
   closeModalF,
-  onBooked
+  onBooked,
 }) => {
-  /*console.log(appointmentData);*/
+
+  const [spinner, setSpinner] = useState(false);
+
   const {
     register,
     handleSubmit,
@@ -47,8 +50,7 @@ const BookAppointmentModal: React.FC<props> = ({
   const router = useRouter();
 
   const bookAppointment = async (formData: FieldValues) => {
-    console.log(formData);
-
+    setSpinner(true)
     const token = localStorage.getItem("sacaturno_token");
     const userID = localStorage.getItem("sacaturno_userID");
     const authHeader = {
@@ -75,8 +77,9 @@ const BookAppointmentModal: React.FC<props> = ({
       bookingData,
       authHeader
     );
-    onBooked()
-    closeModal()
+    onBooked();
+    setSpinner(false)
+    closeModal();
     router.refresh();
   };
 
@@ -85,9 +88,11 @@ const BookAppointmentModal: React.FC<props> = ({
   };
 
   const handleSubmitClick = () => {
+    
     const fileInput = document.querySelector(
       ".inputSubmitField"
     ) as HTMLElement;
+
     if (fileInput != null) {
       fileInput.click();
     }
@@ -97,93 +102,107 @@ const BookAppointmentModal: React.FC<props> = ({
     <>
       <div className="absolute flex items-center justify-center modalCont">
         <div className="flex flex-col bg-white w-80 md:w-96 p-7 h-fit borderShadow">
-          <IoMdClose
-            className={styles.closeModal}
-            onClick={closeModal}
-            size={22}
-          />
-          <h4 className="mb-6 text-xl font-bold text-center uppercase ">
-            Reservar turno
-          </h4>
-
-          {/* <span>Hacé click en un turno para ver los detalles</span> */}
-          <div className="flex flex-col w-full gap-4 h-fit">
-            <div className="flex flex-col w-fit h-fit">
-              <label
-                style={{ fontSize: "12px" }}
-                className="font-bold uppercase "
-              >
-                Fecha y hora
-              </label>
-              <span className="text-sm">
-                {appointmentData?.start} 
-                {appointmentData?.end}
-              </span>
+          {spinner && (
+            <div style={{width:'328px', height:'343px'}} className="flex items-center justify-center w-full h-full bg-white">
+              <div className="loader z-50"></div>
             </div>
+          )}
 
-            <form
-              onSubmit={handleSubmit((formData) => {
-                bookAppointment(formData);
-              })}
-              className="flex flex-col justify-between w-full gap-4 "
-            >
-              <div className={styles.formInput}>
-                <span
-                  style={{ fontSize: "12px" }}
-                  className="font-bold uppercase "
-                >
-                  Nombre y apellido
-                </span>
-                <input type="text" maxLength={30} {...register("name")} />
-                {errors.name?.message && (
-                  <span className="text-xs font-semibold text-red-600">
-                    {" "}
-                    {errors.name.message}{" "}
-                  </span>
-                )}
-              </div>
-              <div className={styles.formInput}>
-                <span
-                  style={{ fontSize: "12px" }}
-                  className="font-bold uppercase "
-                >
-                  Teléfono
-                </span>
-                <input type="number" maxLength={20} {...register("phone")} />
-                {errors.phone?.message && (
-                  <span className="text-xs font-semibold text-red-600">
-                    {" "}
-                    {errors.phone?.message}{" "}
-                  </span>
-                )}
-              </div>
-              <div className={styles.formInput}>
-                <span
-                  style={{ fontSize: "12px" }}
-                  className="font-bold uppercase "
-                >
-                  Email
-                </span>
-                <input type="email" {...register("email")} maxLength={40} />
-                {errors.email?.message && (
-                  <span className="text-xs font-semibold text-red-600">
-                    {" "}
-                    {errors.email?.message}{" "}
-                  </span>
-                )}
-              </div>
-              <button
-                onClick={handleSubmitClick}
-                className={"inputSubmitField hidden "}
+          {!spinner && (
+            <>
+              <IoMdClose
+                className={styles.closeModal}
+                onClick={closeModal}
+                size={22}
               />
-            </form>
-          </div>
+              <h4 className="mb-6 text-xl font-bold text-center uppercase ">
+                Reservar turno
+              </h4>
 
-          <div className="flex justify-center w-full align-middle mt-7 h-fit">
-            <button className={styles.button} onClick={handleSubmitClick}>
-              Reservar turno
-            </button>
-          </div>
+              {/* <span>Hacé click en un turno para ver los detalles</span> */}
+              <div className="flex flex-col w-full gap-4 h-fit">
+                <div className="flex flex-col w-fit h-fit">
+                  <label
+                    style={{ fontSize: "12px" }}
+                    className="font-bold uppercase "
+                  >
+                    Fecha y hora
+                  </label>
+                  <span className="text-sm">
+                    {appointmentData?.start}
+                    {appointmentData?.end}
+                  </span>
+                </div>
+
+                <form
+                  onSubmit={handleSubmit((formData) => {
+                    bookAppointment(formData);
+                  })}
+                  className="flex flex-col justify-between w-full gap-4 "
+                >
+                  <div className={styles.formInput}>
+                    <span
+                      style={{ fontSize: "12px" }}
+                      className="font-bold uppercase "
+                    >
+                      Nombre y apellido
+                    </span>
+                    <input type="text" maxLength={30} {...register("name")} />
+                    {errors.name?.message && (
+                      <span className="text-xs font-semibold text-red-600">
+                        {" "}
+                        {errors.name.message}{" "}
+                      </span>
+                    )}
+                  </div>
+                  <div className={styles.formInput}>
+                    <span
+                      style={{ fontSize: "12px" }}
+                      className="font-bold uppercase "
+                    >
+                      Teléfono
+                    </span>
+                    <input
+                      type="number"
+                      maxLength={20}
+                      {...register("phone")}
+                    />
+                    {errors.phone?.message && (
+                      <span className="text-xs font-semibold text-red-600">
+                        {" "}
+                        {errors.phone?.message}{" "}
+                      </span>
+                    )}
+                  </div>
+                  <div className={styles.formInput}>
+                    <span
+                      style={{ fontSize: "12px" }}
+                      className="font-bold uppercase "
+                    >
+                      Email
+                    </span>
+                    <input type="email" {...register("email")} maxLength={40} />
+                    {errors.email?.message && (
+                      <span className="text-xs font-semibold text-red-600">
+                        {" "}
+                        {errors.email?.message}{" "}
+                      </span>
+                    )}
+                  </div>
+                  <button
+                    onClick={handleSubmitClick}
+                    className={"inputSubmitField hidden "}
+                  />
+                </form>
+              </div>
+
+              <div className="flex justify-center w-full align-middle mt-7 h-fit">
+                <button className={styles.button} onClick={handleSubmitClick}>
+                  Reservar turno
+                </button>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </>
