02357e8348701480e910143cd9669e0d9c23bd7a
Lea <leandrotosunian@hotmail.com>
2024-05-13 21:25:52 -0300

    .

--- config/axios.tsx
+++ config/axios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosReq = axios.create({
-    baseURL: `http://127.0.0.1:4000/api` || 'https://sacaturno-server-production.up.railway.app/api'
+    baseURL: 'https://sacaturno-server-production.up.railway.app/api'
 })
 
 export default axiosReq
