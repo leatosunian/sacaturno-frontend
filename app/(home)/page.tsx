8ef011a52448a5ffac0dcfede399f445124082de
Lea <leandrotosunian@hotmail.com>
2026-02-06 14:08:13 -0300

    fixed back page bug in home/register/login. animatepresence wrap removed from home page.tsx

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -12,130 +12,128 @@ import CallToAction from "@/components/home/CallToAction";
 export default function Home() {
   return (
     <>
-      <AnimatePresence>
-        <motion.div
-          initial={{ opacity: 0 }}
-          exit={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.6, delay: 0.3, ease: "easeInOut" }}
-        >
-          <HeaderPublicBlack />
-          <HeroSection />
-        </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        exit={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.6, delay: 0.3, ease: "easeInOut" }}
+      >
+        <HeaderPublicBlack />
+        <HeroSection />
+      </motion.div>
 
-        <div
-          style={{
-            width: "40%",
-            height: "1px",
-            background: "rgba(255, 255, 255, 0.2)",
-            margin: "0px auto 10rem auto",
-          }}
-          className="hidden lg:block"
-        ></div>
-        <div
-          style={{
-            width: "40%",
-            height: "1px",
-            background: "rgba(255, 255, 255, 0.2)",
-            margin: "2rem auto",
-          }}
-          className="block overflow-hidden lg:hidden"
-        ></div>
+      <div
+        style={{
+          width: "40%",
+          height: "1px",
+          background: "rgba(255, 255, 255, 0.2)",
+          margin: "0px auto 10rem auto",
+        }}
+        className="hidden lg:block"
+      ></div>
+      <div
+        style={{
+          width: "40%",
+          height: "1px",
+          background: "rgba(255, 255, 255, 0.2)",
+          margin: "2rem auto",
+        }}
+        className="block overflow-hidden lg:hidden"
+      ></div>
 
-        <Features />
-        <div
-          style={{
-            width: "40%",
-            height: "1px",
-            background: "rgba(255, 255, 255, 0.2)",
-            margin: "0px auto 8rem auto",
-          }}
-          className="hidden lg:block"
-        ></div>
-        <div
-          style={{
-            width: "40%",
-            height: "1px",
-            background: "rgba(255, 255, 255, 0.2)",
-            margin: "2rem auto",
-          }}
-          className="block overflow-hidden lg:hidden"
-        ></div>
-        <PricingSection />
+      <Features />
+      <div
+        style={{
+          width: "40%",
+          height: "1px",
+          background: "rgba(255, 255, 255, 0.2)",
+          margin: "0px auto 8rem auto",
+        }}
+        className="hidden lg:block"
+      ></div>
+      <div
+        style={{
+          width: "40%",
+          height: "1px",
+          background: "rgba(255, 255, 255, 0.2)",
+          margin: "2rem auto",
+        }}
+        className="block overflow-hidden lg:hidden"
+      ></div>
+      <PricingSection />
 
-        {/* DIVIDER */}
-        <div
-          style={{
-            width: "40%",
-            height: "1px",
-            background: "rgba(255, 255, 255, 0.2)",
-            margin: "0px auto 6.5rem auto",
-          }}
-          className="hidden overflow-hidden lg:block "
-        ></div>
-        <div
-          style={{
-            width: "40%",
-            height: "1px",
-            background: "rgba(255, 255, 255, 0.2)",
-            margin: "2rem auto",
-          }}
-          className="block overflow-hidden lg:hidden "
-        ></div>
+      {/* DIVIDER */}
+      <div
+        style={{
+          width: "40%",
+          height: "1px",
+          background: "rgba(255, 255, 255, 0.2)",
+          margin: "0px auto 6.5rem auto",
+        }}
+        className="hidden overflow-hidden lg:block "
+      ></div>
+      <div
+        style={{
+          width: "40%",
+          height: "1px",
+          background: "rgba(255, 255, 255, 0.2)",
+          margin: "2rem auto",
+        }}
+        className="block overflow-hidden lg:hidden "
+      ></div>
 
-        {/* DIVIDER */}
-
-
-        <Testimonials />
-
-        {/* DIVIDER */}
-        <div
-          style={{
-            width: "40%",
-            height: "1px",
-            background: "rgba(255, 255, 255, 0.2)",
-            margin: "0px auto 6.5rem auto",
-          }}
-          className="hidden lg:block"
-        ></div>
-        <div
-          style={{
-            width: "40%",
-            height: "1px",
-            background: "rgba(255, 255, 255, 0.2)",
-            margin: "2rem auto",
-          }}
-          className="block lg:hidden"
-        ></div>
-        {/* DIVIDER */}
+      {/* DIVIDER */}
 
 
+      <Testimonials />
 
-        <FAQSection />
-        <div
-          style={{
-            width: "40%",
-            height: "1px",
-            background: "rgba(255, 255, 255, 0.2)",
-            margin: "5.4rem auto",
-          }}
-          className="hidden lg:block"
-        ></div>
-        <div
-          style={{
-            width: "40%",
-            height: "1px",
-            background: "rgba(255, 255, 255, 0.2)",
-            margin: "3.5rem auto",
-          }}
-          className="block lg:hidden"
-        ></div>
+      {/* DIVIDER */}
+      <div
+        style={{
+          width: "40%",
+          height: "1px",
+          background: "rgba(255, 255, 255, 0.2)",
+          margin: "0px auto 6.5rem auto",
+        }}
+        className="hidden lg:block"
+      ></div>
+      <div
+        style={{
+          width: "40%",
+          height: "1px",
+          background: "rgba(255, 255, 255, 0.2)",
+          margin: "2rem auto",
+        }}
+        className="block lg:hidden"
+      ></div>
+      {/* DIVIDER */}
 
 
-        <CallToAction />
 
-        <Footer />
-      </AnimatePresence>
+      <FAQSection />
+      <div
+        style={{
+          width: "40%",
+          height: "1px",
+          background: "rgba(255, 255, 255, 0.2)",
+          margin: "5.4rem auto",
+        }}
+        className="hidden lg:block"
+      ></div>
+      <div
+        style={{
+          width: "40%",
+          height: "1px",
+          background: "rgba(255, 255, 255, 0.2)",
+          margin: "3.5rem auto",
+        }}
+        className="block lg:hidden"
+      ></div>
+
+
+      <CallToAction />
+
+      <Footer />
     </>
   );
 }
