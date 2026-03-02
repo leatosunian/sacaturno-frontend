3035870377bfa95d8cd800b796d9a10442e44d45
Lea <leandrotosunian@hotmail.com>
2026-03-02 04:08:47 -0300

    fixed bug in expired/free alert in formsettings

--- components/dashboard/services/FormSettings.tsx
+++ components/dashboard/services/FormSettings.tsx
@@ -393,9 +393,9 @@ const FormSettings = ({
                 </Button>
               )}
 
-              {subscriptionData.subscriptionType === "SC_FREE" && (
+              {(subscriptionData.subscriptionType === "SC_EXPIRED" || subscriptionData.subscriptionType === "SC_FREE") && (
                 <>
-                  <div className="w-full mx-auto mt-2 mb-1 notifications-container ">
+                  <div className="w-full mx-auto mt-2 mb-1 notifications-container">
                     <div className="alert">
                       <div className="flex">
                         <div className="flex-shrink-0">
@@ -413,55 +413,22 @@ const FormSettings = ({
                             ></path>
                           </svg>
                         </div>
-                        <div className="alert-prompt-wrap">
-                          <p className="text-sm text-yellow-700">
-                            Para agregar más de un servicio debes suscribirte al
-                            Plan Full.{" "}
-                            <span
-                              className="cursor-pointer alert-prompt-link"
-                              onClick={handleMercadoPagoPreference}
-                            >
-                              Actualizar suscripción
-                            </span>
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </>
-              )}
+                        <div className="alert-prompt-wrap flex flex-col items-start gap-2">
+                          {subscriptionData.subscriptionType === "SC_EXPIRED" && (
+                            <p className="text-sm text-yellow-700">  Para agregar más servicios debes suscribirte al
+                              Plan Full.{" "}</p>
+                          )}
+                          {subscriptionData.subscriptionType === "SC_FREE" && (
+                            <p className="text-sm text-yellow-700">Para agregar más de un servicio debes suscribirte al
+                              Plan Full.{" "}</p>
+                          )}
 
-              {subscriptionData.subscriptionType === "SC_EXPIRED" && (
-                <>
-                  <div className="w-full mx-auto mt-5 mb-2 notifications-container md:w-5/6 lg:w-4/6">
-                    <div className="alert">
-                      <div className="flex">
-                        <div className="flex-shrink-0">
-                          <svg
-                            aria-hidden="true"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="w-5 h-5 alert-svg"
+                          <span
+                            className="cursor-pointer alert-prompt-link"
+                            onClick={handleMercadoPagoPreference}
                           >
-                            <path
-                              clipRule="evenodd"
-                              d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
-                              fillRule="evenodd"
-                            ></path>
-                          </svg>
-                        </div>
-                        <div className="alert-prompt-wrap">
-                          <p className="text-sm text-yellow-700">
-                            Para agregar más servicios debes suscribirte al
-                            Plan Full.{" "}
-                            <span
-                              className="cursor-pointer alert-prompt-link"
-                              onClick={handleMercadoPagoPreference}
-                            >
-                              Actualizar suscripción
-                            </span>
-                          </p>
+                            Actualizar suscripción
+                          </span>
                         </div>
                       </div>
                     </div>
