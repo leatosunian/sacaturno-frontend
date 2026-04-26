ccda381708b326fed2e6ee5030626b3ff7b15e3b
Leandro Tosunian <leandrotosunian@hotmail.com>
2026-04-26 00:01:28 -0300

    fix: correct baseURL assignment in axios configuration

--- config/axios.tsx
+++ config/axios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosReq = axios.create({
-  baseURL: 'http://localhost:4000/api' || process.env.NEXT_PUBLIC_BACKEND_URL
+  baseURL:  process.env.NEXT_PUBLIC_BACKEND_URL|| 'http://localhost:4000/api'
 })
 
 export default axiosReq
